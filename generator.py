aa0719cb63f644fec31c195acc74de1e3a393727
Ben Dines <ben@dines.family>
2025-05-08 16:44:18 -0700

    Some cleanup and variable renaming

--- generator.py
+++ generator.py
@@ -1,46 +1,58 @@
+import sys
 import yaml
 from jinja2 import Environment, FileSystemLoader
 import http.server
-import socketserver
 import re
+from pathlib import Path
+import random
 from pprint import pprint
 
 """
 convert curly quotes to straight quotes and em and en dashes to hyphens
 """
+
+
 def clean_quotes_and_dashes(text):
-    if not text: return text
-    text = re.sub(r'“|”', '"', text)
-    text = re.sub(r'‘|’', "'", text)
-    text = re.sub(r'—', '-', text)
-    text = re.sub(r'–', '-', text)
+    if not text:
+        return text
+    text = re.sub(r"“|”", '"', text)
+    text = re.sub(r"‘|’", "'", text)
+    text = re.sub(r"—", "-", text)
+    text = re.sub(r"–", "-", text)
     return text
+
 
 """
 convert curly quotes and em and en dashes to proper HTML code
 """
+
+
 def convert_quotes_and_dashes(text):
-    if not text: return text
+    if not text:
+        return text
     text = text.replace("“", "&#147;")
     text = text.replace("”", "&#148;")
     text = text.replace("‘", "&#145;")
     text = text.replace("’", "&#146;")
     return text
 
+
 """
 flatten lists of dictionaries to 1 list of k,v pairs
 bare lists are (None, <list>)
 scalars are (None, <scalar>)
 """
+
+
 def prep_details(items):
     # if is_str(items):
     #     return items
     result = []
-    isdict = False
+    use_tuples = False
     # if isinstance(items, (dict, tuple)):
     if is_dict(items):
         items = [items]
-        isdict = True
+        use_tuples = True
     elif not is_list(items):
         return items
     # elif len(items) == 1:
@@ -49,18 +61,19 @@ def prep_details(items):
         if is_list(item):
             # if len(item) <= 1:
             #     result.extend(prep_details(item))
-            if isdict:
+            if use_tuples:
                 result.append((None, prep_details(item)))
             else:
                 result.append(prep_details(item))
         elif is_dict(item):
-            isdict = True
+            use_tuples = True
             result.extend((k, prep_details(v)) for k, v in item.items())
         else:
-            result.append((None, item) if isdict else item)
-    # if len(result) == 1 and isdict:
+            result.append((None, item) if use_tuples else item)
+    # if len(result) == 1 and use_tuples:
     #     return result[0]
     return result
+
 
 def is_instance(item, typ):
     # pprint(vars())
@@ -69,17 +82,22 @@ def is_instance(item, typ):
     typ = tuple(globals()[t] if isinstance(t, str) else t for t in typ)
     return isinstance(item, typ)
 
+
 def is_str(item):
     return is_instance(item, str)
+
 
 def is_list(item):
     return is_instance(item, list)
 
+
 def is_tuple(item):
     return is_instance(item, tuple)
 
+
 def is_collection(item):
     return is_instance(item, (list, tuple, set, dict))
+
 
 def is_dict(item):
     return is_instance(item, dict)
